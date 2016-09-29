10922bc4e5a716028b641648bb60e4e341c00c26
Mart <mvosumagi@gmail.com>
2016-09-29 18:20:13 +0300

    kk

--- src/main/main.java
+++ src/main/main.java
@@ -1,5 +1,5 @@
 public class main {
     public static void main(String args[]) {
-        System.out.println("Elekter sisse/välja");
+        System.out.println("Elekter sisse/välja prooviks veel");
     }
 }
