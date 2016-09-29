8b6ac179f73e2b2ef9b67721a584e594dd0c3dec
Mart <mvosumagi@gmail.com>
2016-09-29 20:14:40 +0300

    fff

--- src/main/main.java
+++ src/main/main.java
@@ -1,17 +1,5 @@
 public class main {
     public static void main(String args[]) {
-<<<<<<< HEAD
-        System.out.println("Elekter sisse/välja prooviks veel");
-=======
-        System.out.println("Elekter sisse/välja");
-        System.out.println("Elekter sisse/välja");
-        System.out.println("Elekter sisse/välja");
-System.out.println("Elekter sisse/välja");
-System.out.println("Elekter sisse/välja");
-
-
-
-
->>>>>>> e47d043d7889bdf15cdf3e9e1d67f32fed7049db
+        System.out.println("Elekter sisse valja prooviks!");
     }
 }
