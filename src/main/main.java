78476ade9bf513e7676811c3bd1485b6dcf14040
Mart <mvosumagi@gmail.com>
2016-09-30 19:59:19 +0300

    ideas tehtud muudatused

--- src/main/main.java
+++ src/main/main.java
@@ -1,5 +1,11 @@
 public class main {
-    public static void main(String args[]) {
+    public static void main(String args[])
+    {
         System.out.println("Elekter sisse valja prooviks!");
+        System.out.println("Elekter sisse valja prooviks!");
+        System.out.println("Elekter sisse valja prooviks!");
+        System.out.println("Elekter sisse valja prooviks!");
+        System.out.println("Elekter sisse valja prooviks!");
+
     }
 }
