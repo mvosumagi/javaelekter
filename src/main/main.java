d08e4cf3449fecd785064f212daba8016d4bfb25
Mart <mvosumagi@gmail.com>
2016-10-09 12:11:23 +0300

    Prjekti kijeldamine 20161009

--- src/main/main.java
+++ src/main/main.java
@@ -1,13 +1,9 @@
 public class main {
     public static void main(String args[])
     {
-        System.out.println("Elekter sisse valja prooviks!");
-        System.out.println("Elekter sisse valja prooviks!");
-        System.out.println("Elekter sisse valja prooviks!");
-        System.out.println("Elekter sisse valja prooviks!");
-        System.out.println("Elekter sisse valja prooviks!");
-
-
+        System.out.println("Elekter sisse valja.");
+        System.out.println("Mart V.");
+        System.out.println("2016");
 
    }
 }
